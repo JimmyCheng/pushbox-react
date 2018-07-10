83063c915b47ce16cce4d8acc0f2a8fbe56853be
Jimmy Cheng <jimmy@orchestrated.io>
2018-07-10 22:51:53 +1000

    replace the task file with json file.

--- src/utility/taskReader.js
+++ src/utility/taskReader.js
@@ -5,14 +5,54 @@ const taskReader = (path, taskId) => {
   console.log(fileName);
 
   let buf = fs.readFileSync(fileName, null);
-  let task = [];
+  let matrix = [];
+
+  //build a 14*16 matrix
   if(buf.length === 224) {
-    for (let i = 0; i < 224; i++) {
-      task.push(buf.readInt8(i));
+    for (let i = 0; i < 14; i++) {
+      let row = [];
+      for(let j = 0; j< 16; j++) {
+        const val = buf.readInt8(i * 16 + j);
+        row.push(val);
+      }
+      matrix.push(row);
     }
   }
-  return task;
+  return matrix;
 };
+
+const writeToJson = (matrix, path, taskId) => {
+  //to make it more readable, use following routine rather than JSON.stringify.
+  let json =`{\n`;
+  json = json.concat(`  "matrix": [\n`);
+
+  for (let i = 0; i < 14; i++) {
+    json = json.concat(`    [`);
+    for (let j = 0; j < 15; j++) {
+      json = json.concat(`${matrix[i][j]},`);
+    }
+    json = json.concat(`${matrix[i][15]}]`);
+
+    if(i != 13){
+      json = json.concat(`,`);
+    }
+    json = json.concat(`\n`);
+  }
+  json = json.concat(`  ]\n`);
+  json = json.concat(`}\n`);
+
+  fs.writeFile(`${path}/task${taskId}.json`, json,
+    function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+};
+
+//conver the binary task to task.json.
+// for(let i=1; i<=100; i++) {
+//   writeToJson(taskReader(__dirname, i), __dirname, i);
+// }
 
 export default taskReader;
 
