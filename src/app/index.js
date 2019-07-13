41b358043d20c4e451c3be30fc5b57958db8cc7b
Jimmy Cheng <chengnianhua@gmail.com>
2019-07-13 23:24:50 +1000

    Add BrowserRouter to the routing.

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 //reference: https://reacttraining.com/react-router/web/example/basic
 
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
@@ -9,17 +10,19 @@ import TasksPage from "../pages/TasksPage";
 
 const App = props => {
   return (
-    <Switch>
-      <Route exact path="/" render={({ match }) => {
-        return <HomePage {...props} match={match} />;
-      }}/>
-      <Route exact path="/about" render={({ match }) => {
-        return <AboutPage {...props} match={match} />;
-      }}/>
-      <Route exact path="/tasks" render={({ match }) => {
-        return <TasksPage {...props} match={match} />;
-      }}/>
-    </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" render={({ match }) => {
+            return <HomePage {...props} match={match} />;
+          }}/>
+          <Route exact path="/about" render={({ match }) => {
+            return <AboutPage {...props} match={match} />;
+          }}/>
+          <Route exact path="/tasks" render={({ match }) => {
+            return <TasksPage {...props} match={match} />;
+          }}/>
+        </Switch>
+      </Router>
   );
 };
 
