c2ef36099762f56131442a4b22b09ff416b4315e
Jimmy Cheng <jimmy@orchestrated.io>
2020-01-23 16:54:18 +1100

    Code format change.

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
-PUSH BOX
-===========================
+# PUSH BOX
+
 This is a React version of the push box game.
 The intention of this game is to demonstrate mainstream react technology and devops.
-****
-	
-|Author|Jimmy|
-|---|---
-|E-mail|chengnianhua@gmail.com
 
-****
+---
+
+| Author | Jimmy                  |
+| ------ | ---------------------- |
+| E-mail | chengnianhua@gmail.com |
+
+---
+
 ## Table Content
-* [How to Play](#how-to-play)
-* [How to Install](#how-to-install)
-* [Design Highlight](#design-highlight)
-* [Other Implementations](#other-implementations)
 
-How To Play
------------
+- [How to Play](#how-to-play)
+- [How to Install](#how-to-install)
+- [Design Highlight](#design-highlight)
+- [Other Implementations](#other-implementations)
+
+## How To Play
+
 The game is simple, use key "up", "left", "right", "down" to move the little man around. Use key "u" to undo a step. e.g. for the task 10, it is displayed:
 
 <img src="Task10.png" alt="Task10" title="Task 10" width="600" height="600">
@@ -28,18 +31,20 @@ then the task 88 is displayed:
 
 <img src="Task88.png" alt="Task88" title="Task 88" width="600" height="600">
 
-How To Install
------------
+## How To Install
+
 Clone the git respository use `git clone` and import the project in eclipse or intelij.
 
-Design Highlight
------------
+## Design Highlight
+
 The game is controlled by a matrix:
+
 ```Javascript
 grid = new Cell[14][16];  //14 rows and 16 columns.
 ```
 
 Each cell is an object with current status:
+
 ```Java
 public class Cell {
     boolean black;
@@ -61,11 +66,15 @@ public class Cell {
     }
 }
 ```
+
 In terms of the MVC design pattern, there the `grid` is the Model, the `canvas` is the view, the `keyUp/Down` event is the controller.
 
-Other Implementations
------------
+## References
+
+1. https://github.com/TarikHuber/react-most-wanted
+
+## Other Implementations
 
 #### Delphi Version
-#### React Version
 
+#### React Version
--- src/container/game/index.js
+++ src/container/game/index.js
@@ -1,24 +1,36 @@
-import { compose, withProps, withState, withHandlers, shouldUpdate, withPropsOnChange } from "recompose";
+import {
+  compose,
+  withProps,
+  withState,
+  withHandlers,
+  shouldUpdate,
+  withPropsOnChange
+} from "recompose";
 import tasks from "../../consts/tasks";
 import matrixParser from "../../utility/matrixParser";
 import Game from "../../components/game";
 
 const withGameData = compose(
-    withProps( ({match}) => {return {initialTask: match.params.id}}),
-  withState("taskId", "updateTaskId", ({initialTask}) => initialTask),
+  withProps(({ match }) => {
+    return { initialTask: match.params.id };
+  }),
+  withState("taskId", "updateTaskId", ({ initialTask }) => initialTask),
   withState("retry", "updateRetry", 1),
   withState("steps", "updateSteps", 0),
   withHandlers({
-    handleNextTask: ({ updateTaskId}) => () => updateTaskId(n => n + 1),
-    handlePrevTask: ({ updateTaskId }) => () => updateTaskId(n => n-1 > 0 ? n - 1: n),
+    handleNextTask: ({ updateTaskId }) => () => updateTaskId(n => n + 1),
+    handlePrevTask: ({ updateTaskId }) => () =>
+      updateTaskId(n => (n - 1 > 0 ? n - 1 : n)),
     handleReplay: ({ updateRetry }) => () => updateRetry(n => n + 1),
-    handleSteps: ({ updateSteps }) => () => updateSteps(n => n + 1),
+    handleSteps: ({ updateSteps }) => () => updateSteps(n => n + 1)
   }),
-  shouldUpdate(((props, nextProps) => {
-    return props.taskId !== nextProps.taskId ||
+  shouldUpdate((props, nextProps) => {
+    return (
+      props.taskId !== nextProps.taskId ||
       props.retry !== nextProps.retry ||
-      props.steps !== nextProps.steps;
-  })),
+      props.steps !== nextProps.steps
+    );
+  }),
 
   withState("grid", "updateGrid"),
   withState("currX", "updateCurrX"),
@@ -27,11 +39,19 @@ const withGameData = compose(
   withState("history", "updateHistory", []),
 
   withPropsOnChange(["taskId", "retry"], props => {
-    const { taskId, updateGrid, updateCurrX, updateCurrY, updateBoxCount, updateHistory, updateSteps } = props;
-    if(taskId) {
+    const {
+      taskId,
+      updateGrid,
+      updateCurrX,
+      updateCurrY,
+      updateBoxCount,
+      updateHistory,
+      updateSteps
+    } = props;
+    if (taskId) {
       const matrix = tasks[`task${taskId}`];
       console.log("the matrix is===>", matrix);
-      const task = {...matrixParser(matrix), isLoading: false};
+      const task = { ...matrixParser(matrix), isLoading: false };
       console.log("the task is===>", task);
       updateGrid(task.grid);
       updateCurrX(task.currX);
