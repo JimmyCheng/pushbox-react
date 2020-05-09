b7887636cd6fa36cbcbf3d003b9aee924057849d
Jimmy Cheng <jimmy@orchestrated.io>
2020-05-09 22:48:18 +1000

    Update the home page to display all the tasks.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { map, range } from "lodash";
 
-const HomePage = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
+const Task = ({ task, history }) => {
+  const onClickTask = () => {
+    history.push(`/task/${task}`);
+  };
+
+  return <button onClick={onClickTask}>task {task}</button>;
+};
+const HomePage = () => {
+  const history = useHistory();
+  const tasks = range(1, 101);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      {map(tasks, task => (
+        <Task key={task} history={history} task={task} />
+      ))}
+    </div>
+  );
+};
 
 export default HomePage;
